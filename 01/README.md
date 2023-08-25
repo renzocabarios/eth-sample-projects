d2c42c70657404023084223308cad6831379b34c
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-08-25 22:32:58 +0800

    Add 01

---
+++ 01/README.md
@@ -0,0 +1,12 @@
+# Sample Hardhat Project
+
+- configure hardhat config
+- run `npx hardhat compile`
+- run `npx hardhat run ./scripts/deploy.ts --network sepolia`
+- run `npx hardhat verify --contract contracts/Project01.sol:Project01 --network sepolia <program-id>`
+
+## DESCRIPTION
+
+```
+A wallet can only mint once. You cannot mint more than once even if you pay ETH
+```
---
+++ 01/contracts/Project01.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Project01 is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+    mapping(address => bool) public walletsMinted;
+    uint public MINT_PRICE = 100;
+
+    constructor() ERC721("NFTTutorial", "NFT") {}
+
+    function mint() public payable returns (uint256) {
+        require(walletsMinted[msg.sender] == true, "Wallet has already minted");
+        require(MINT_PRICE == msg.value, "Mint costs a 100 WEI");
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(msg.sender, newItemId);
+        return newItemId;
+    }
+}
